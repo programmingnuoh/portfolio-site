82f773670bf973b0703e064f9f16ae38fb0e7fca
programmingnuoh <myth0725@gmail.com>
2021-07-24 21:56:22 +0900

    各SKILLにCSS追加

--- app/javascript/packs/move.js
+++ app/javascript/packs/move.js
@@ -55,10 +55,24 @@ function sentenceUnderline(){
   });
 };
 
+function skillFrip(){
+  $('.skill-content').each(function(){
+    const elemPos = $(this).offset().top-50;
+    const scroll = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('flipRightTop')
+    } else {
+      $(this).removeClass('flipRightTop')
+    }
+  });
+};
+
 $(window).scroll(function(){
   welcomeTitleFade();
   titleFade();
   timeArticleFade();
   sentenceUnderline();
+  skillFrip();
 });
 
